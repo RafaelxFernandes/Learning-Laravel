75b4df8ea3acc50e5dc861d888a48ce0be59d879
RafaelxFernandes <rafaelfernandes@dcc.ufrj.br>
2018-07-19 18:20:47 -0300

    Consertar problema do nullable

---
+++ database/migrations/2018_07_19_182540_create_produtos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->increments('id')->nullable();
+            $table->string('nome');
+            $table->string('tipo')->default('flor');
+            $table->float('preco');
+            $table->integer('quantidade')->unsigned();
+            $table->integer('id_cliente')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
